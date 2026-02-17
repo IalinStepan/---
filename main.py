1bf8373e7d774e00ad35f3c1619a9fc4bcc40fe1
IalinStepan <ialinstepan123@gmail.com>
2026-02-17 15:50:43 +0700

    init: first class and project structure

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ class TemperatureSensor:
         self.max_temp = max_temp
         self.accuracy = accuracy
     def info(self):
-        print(f"Датчик {self.sensor_id} ({type}): диапазон {self.min}..{self.max} °C, точность ±{self.accuracy} °C")
-TemperatureSensor1 = TemperatureSensor("1", )
-
+        print(f"Датчик {self.sensor_id} ({self.type}): диапазон {self.min_temp}..{self.max_temp} °C, точность ±{self.accuracy} °C")
+TemperatureSensor1 = TemperatureSensor("1", 1, 150, 350, 7)
+TemperatureSensor2 = TemperatureSensor("2", 2, 250, 450, 6)
+TemperatureSensor3 = TemperatureSensor("3", 3, 350, 550, 5)
+TemperatureSensor1.info()
+TemperatureSensor2.info()
+TemperatureSensor3.info()
 
 
 
