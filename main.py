a93a774787f248787002f01943867c73fde3b269
IalinStepan <ialinstepan123@gmail.com>
2026-02-17 15:40:28 +0700

    init: first class and project structure

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+class TemperatureSensor:
+    def __init__(self, sensor_id, type, min_temp, max_temp, accuracy):
+        self.sensor_id = sensor_id
+        self.type = type
+        self.min_temp = min_temp
+        self.max_temp = max_temp
+        self.accuracy = accuracy
+    def info(self):
+        print(f"Датчик {self.sensor_id} ({type}): диапазон {self.min}..{self.max} °C, точность ±{self.accuracy} °C")
+TemperatureSensor1 = TemperatureSensor("1", )
+
+
+
+
+
+
+    
